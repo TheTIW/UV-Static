844386b3a2b346dac50d253ce366520fe9b2449b
Kwazy <124801774+KwazyMotoo@users.noreply.github.com>
2023-06-15 15:07:06 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: 'https://0ziuro-6969.csb.app/',
+    bare: 'http://baresw.starttiw.org/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
