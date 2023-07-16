e9dfda8ea8a2a4238f50ae78873fdf23b4e2322d
Kwazy <124801774+KwazyMotoo@users.noreply.github.com>
2023-07-16 17:28:32 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare: 'https://lmath.lolmath.tk/bare/',
+    bare: 'https://tiw.tiwweb.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
